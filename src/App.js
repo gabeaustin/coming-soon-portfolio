f2887bb233d3985e38983f8b3b1287fadf22e24d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-29 18:12:21 -0500

    updated media queries for main text on home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
       <div>
-        <div className="mainPageDiv">
+        <div className="mainPageDiv col-md-12">
           <span className="orange">import</span>{" "}
-          <span className="yellow">&#123;</span>{" "}
-          <span className="white">ComingSoon</span>{" "}
+          <span className="yellow">&#123;</span>
+          <span className="white">ComingSoon</span>
           <span className="yellow">&#125;</span>{" "}
           <span className="orange">from</span>{" "}
-          <span className="green">"gabrielAustin"</span>
+          <span className="green">"gabeAustin"</span>
           <span className="grey">;</span>
         </div>
         <footer className="mainPageFooter p-3 mt-auto">
